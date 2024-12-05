d6d11309782923c4df8dd2bb63d01243ab3ef81c
Idrissa Baby <ibaby@made-f0Ar8s11.clusters.42paris.fr>
2024-12-05 14:54:11 +0100

    final

--- test.c
+++
@@ -1,9 +0,0 @@
-#include "includes/ft_printf.h"
-
-int main()
-{
-	int real = printf("[%1s]\n", "");
-	int fake = ft_printf("[%1s]\n", "");
-	printf( "\nprintf =    %d\n", real);
-	printf( "ft_printf = %d\n", fake);
-}
